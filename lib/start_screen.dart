b8e644b2f0157437d8ea0ae5690546044a20cda5
johnnathan <johnnathanfreire@gmail.com>
2024-10-16 17:24:00 -0300

    Refactor: link the button to the other screen

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -12,7 +14,7 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 300,
-            color: const Color.fromARGB(125, 255, 255, 255),
+            color: const Color.fromARGB(150, 255, 255, 255),
           ),
           /* Opacity(
             opacity: 0.6,
@@ -31,7 +33,7 @@ class StartScreen extends StatelessWidget {
           ),
           const SizedBox(height: 30),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
