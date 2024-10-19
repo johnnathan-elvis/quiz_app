2e022612171b5c804a467a6b5d136b2d3beda5b8
johnnathan <johnnathanfreire@gmail.com>
2024-10-19 18:38:52 -0300

    Refactor: linking the result screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+
+import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -12,7 +15,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
   //var activeScreen = 'start-screen';
   Widget? activeScreen;
 
@@ -24,12 +27,22 @@ class _QuizState extends State<Quiz> {
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = ResultsScreen(
+          chosenAnswers: selectedAnswers,
+        );
+      });
+    }
   }
 
   void switchScreen() {
     setState(() {
       //activeScreen = 'questions-screen';
-      activeScreen = QuestionsScreen(onSelectAnswer: chooseAnswer,);
+      activeScreen = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
     });
   }
 
